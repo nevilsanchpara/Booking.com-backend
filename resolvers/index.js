42f5dece69cc5d3a66762fe2d98b8bda05faeba4
Nevil Saspara <81670918+nevilsanchpara@users.noreply.github.com>
2024-06-15 00:08:59 -0400

    code

--- resolvers/index.js
+++
@@ -1,9 +0,0 @@
-const roomResolvers = require("./room.resolver");
-const hotelResolvers = require("./hotel.resolver");
-
-module.exports = {
-  Query: {
-    ...roomResolvers.Query,
-    ...hotelResolvers.Query,
-  },
-};
